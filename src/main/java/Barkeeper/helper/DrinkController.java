1e5665964eec1bb6c64bd9a35e2f2684e362e944
KingChaos16 <smpcraft12345@gmail.com>
2025-04-25 12:08:04 +0200

    GEt Router

---
+++ src/main/java/Barkeeper/helper/DrinkController.java
@@ -0,0 +1,13 @@
+package Barkeeper.helper;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DrinkController {
+
+    @GetMapping("/drinks")
+    public String getDrinks() {
+        return "List of drinks";
+    }
+}
